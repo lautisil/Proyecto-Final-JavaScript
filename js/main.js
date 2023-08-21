82be8d7092c5c39215a22fc133b7c29a2dc75853
Jorge Lautaro Silvestro <lautarosilvestro2000@hotmail.com>
2023-08-21 20:36:07 -0300

    array y objeto

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ function membresia() {
     for (let i = 3; i > 0; i--) { 
       contraseñaIngresada = prompt("Introduzca su contraseña: ");
       if (contraseñaIngresada == contraseña) {
-        alert("Bievenido Juan! Tienes acceso a todos nuestros descuentos. Y un bono de $20.000 para tus primeras compras.");
+        alert("Bievenido Juan Perez! Tienes acceso a todos nuestros descuentos. Y un bono de $20.000 para tus primeras compras.");
         miembro = true;
         break;
       } else {
@@ -23,29 +23,37 @@ function membresia() {
   }
 }
 
+
 membresia()
 
 /* PRODUCTOS */
-let natRojo = 15000;
-let natAzul = 12000;
-let natVerde = 8000;
-let natVioleta = 23000;
-if (miembro) {
-  natRojo *= 0.8;
-  natAzul *= 0.8;
-  natVerde *= 0.8;
-  natVioleta *= 0.8;
+class producto {                                            //OBJETO
+  constructor(nombre, tipo, precio, stock) {
+    this.nombre = nombre;
+    this.tipo = tipo;
+    this.precio = precio;
+    this.stock = stock;
+  }
+  esMiembro() {
+    return this.precio *= 0.8;
+  }
 }
 
-/* SALDO */
+const perfumeRojo = new producto("Perfume", "Rojo", 20000, 5);
+const perfumeAzul = new producto("Perfume", "Azul", 15000, 8);
+const cremaVerde = new producto("Crema", "Verde", 10000, 12);
+const cremaVioleta = new producto("Crema", "Violeta", 6000, 23);
+
+/* SALDO / CARRITO / STOCK */
 let saldo = 0;
 let carrito = 0;
-if (miembro) {
+if (miembro) { //saldo como miembro
   saldo += 20000;
 }
+const listaCarrito = [] //Array
 
 
-let opcion = prompt("Elija lo que desee hacer a continuacion: \n 1- Comprar ofertas exclusivas de la semana. \n 2- Carrito. \n 3- Depositar. \n 4- Pagar.\n 5- Salir");
+let opcion = prompt("Elija lo que desee hacer a continuacion: \n 1- Comprar ofertas exclusivas de la semana. \n 2- Carrito. \n 3- Depositar. \n 4- Pagar. \n 5- Salir");
 while (opcion != 5) {
   switch(opcion) {
     case "1":
@@ -54,36 +62,40 @@ while (opcion != 5) {
       } else {
         alert("Recuerda que siendo miembro de nuestra tienda podras acceder a descuentos exclusivos.")
       }
-      let compraPerfume = prompt("Durante esta semana tendremos a tu disposicion estos perfumes: \n1. Natura Rojo - " + natRojo + "\n2. Natura Azul - "+ natAzul +" \n3. Natura Verde - "+ natVerde +" \n4. Natura Violeta - " + natVioleta + "\nSi desea comprar alguno de nuestros productos introduzca el numero del articulo a continuacion: \nIntroduzca '0' para terminar de comprar."  );
+      let compraPerfume = prompt("Durante esta semana tendremos a tu disposicion estos perfumes: \n1. Natura Rojo - " + perfumeRojo.esMiembro + "\n2. Natura Azul - "+ perfumeAzul.precio +" \n3. Natura Verde - "+ cremaVerde.precio +" \n4. Natura Violeta - " + cremaVioleta.precio + "\nSi desea comprar alguno de nuestros productos introduzca el numero del articulo a continuacion: \nIntroduzca '0' para terminar de comprar.");
 
       while (compraPerfume != 0) {
         switch (compraPerfume) {
           case "1":
-            carrito += natRojo;
+            carrito += perfumeRojo.precio;
+            listaCarrito.push("\n"+ perfumeRojo.precio +" "+ perfumeRojo.tipo);
             alert("Carrito: " + carrito);
             break;
           case "2":
-            carrito += natAzul;
+            carrito += perfumeAzul.precio;
+            listaCarrito.push("\n"+ perfumeAzul.nombre +" "+ perfumeAzul.tipo);
             alert("Carrito: " + carrito);
             break;
           case "3":
-            carrito += natVerde;
+            carrito += cremaVerde.precio;
+            listaCarrito.push("\n"+ cremaVerde.nombre +" "+ cremaVerde.tipo);
             alert("Carrito: " + carrito);
             break;
           case "4":
-           carrito += natVioleta;
+           carrito += cremaVioleta.precio;
+           listaCarrito.push("\n"+ cremaVioleta.nombre +" "+ cremaVioleta.tipo);
            alert("Carrito: " + carrito);
            break;
           default:
             alert("Opcion no valida");
             break;
         }
-        compraPerfume = prompt("Por esta semana tenemos en 20% de descuento los siguientes perfumes. \n1. Natura Rojo - " + natRojo + "\n2. Natura Azul - "+ natAzul +" \n3. Natura Verde - "+ natVerde +" \n4. Natura Violeta - " + natVioleta + "\nSi desea comprar alguno de nuestros productos introduzca el numero del articulo a continuacion: \nIntroduzca '0' para terminar de comprar."  );
+        compraPerfume = prompt("Durante esta semana tendremos a tu disposicion estos perfumes: \n1. Natura Rojo - " + perfumeRojo.precio + "\n2. Natura Azul - "+ perfumeAzul.precio +" \n3. Natura Verde - "+ cremaVerde.precio +" \n4. Natura Violeta - " + cremaVioleta.precio + "\nSi desea comprar alguno de nuestros productos introduzca el numero del articulo a continuacion: \nIntroduzca '0' para terminar de comprar.");
       }
       break;
 
     case "2":
-      alert("Su monto en el carrito es de: " + carrito);
+      alert("Su monto en el carrito es de: " + carrito + "\nCantidad de productos: " + listaCarrito.length + ".\n" + listaCarrito);
       break;
 
     case "3":
@@ -99,8 +111,12 @@ while (opcion != 5) {
           if (carrito > saldo) {
             alert("El saldo en su cuenta es inferior al monto que tiene en el carrito. Deposite plata.")
             continue;
+          } else if (carrito == 0){
+            alert("No tiene nada en el carrito para pagar.")
+            break;
           } else {
             saldo -= carrito
+            carrito = 0
             alert("Su nuevo saldo en su cuenta es de: " + saldo);
             break;
           }
@@ -113,11 +129,11 @@ while (opcion != 5) {
           continue;
       }
       break;
-    
+
     default:
       alert("Elija una opcion valida.");
       break;
 
   }
-  opcion = prompt("Elija lo que desee hacer a continuacion: \n 1- Comprar ofertas exclusivas de la semana. \n 2- Carrito. \n 3- Depositar. \n 4- Pagar.\n 5- Salir");
+  opcion = prompt("Elija lo que desee hacer a continuacion: \n 1- Comprar ofertas exclusivas de la semana. \n 2- Carrito. \n 3- Depositar. \n 4- Pagar. \n 5- Salir");
 }
