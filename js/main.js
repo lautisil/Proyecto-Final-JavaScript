8a2a98b94aec324d148c0981c3adcf4c1e9c45b0
Jorge Lautaro Silvestro <lautarosilvestro2000@hotmail.com>
2023-09-07 18:50:04 -0300

    entrega3

--- js/main.js
+++ js/main.js
@@ -1,157 +1,126 @@
-import Swal from 'sweetalert2'
-/* VARIABLES */
-let saldo = 0;
-let carrito = 0;
+const btnCart = document.querySelector('.container-cart-icon');
+const containerCartProducts = document.querySelector(
+	'.container-cart-products'
+);
 
-/* CLASS Y OBJETOS */
-class Producto {
-  constructor(id, nombre, tipo, precio, stock) {
-    this.id = id;
-    this.nombre = nombre;
-    this.tipo = tipo;
-    this.precio = precio;
-    this.stock = stock;
-  };
-};
-
-const perfumeRojo = new Producto(1, "Perfume", "Rojo", 20000, 5);
-const perfumeAzul = new Producto(2, "Perfume", "Azul", 15000, 8);
-const cremaVerde = new Producto(3, "Crema", "Verde", 10000, 12);
-const cremaVioleta = new Producto(4, "Crema", "Violeta", 6000, 23);
-
-/* ARRAYS */
-const listaCarrito = [];
-const productosNatura = [perfumeRojo, perfumeAzul, cremaVerde, cremaVioleta]
-
-/* FUNCIONES */
-function agregarAlCarrito(producto) {
-  carrito += producto.precio;
-  listaCarrito.push(producto);
-  alert("Producto agregado al carrito: " + producto.nombre + " " + producto.tipo);
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  const botonesCompra = document.querySelectorAll(".btn-submit");
-
-  botonesCompra.forEach((boton, index) => {
-      boton.addEventListener("click", () => {
-          agregarAlCarrito(productosNatura[index]);
-          
-      });
-  });
+btnCart.addEventListener('click', () => {
+	containerCartProducts.classList.toggle('hidden-cart');
 });
 
-function mostrarCarrito() {
-  if (listaCarrito.length === 0) {
-      Swal.fire({
-          icon: 'error',
-          title: 'No hay nada en el carrito.',
-      })
-  } else {
-      let mensaje = "Productos en el carrito:\n";
-      mensaje += listaCarrito.map((el) => {
-          return "ID: " + el.id + " - " + el.nombre + " " + el.tipo + "- $" + el.precio + " pesos.";
-      }).join("\n")
-      mensaje += "\n\nTotal en el carrito: $" + carrito + " pesos.";
+/* ========================= */
+const cartInfo = document.querySelector('.cart-product');
+const rowProduct = document.querySelector('.row-product');
 
-      // Mostrar los productos en el div "carritoContenedor"
-      const carritoContenedor = document.getElementById("carritoContenedor");
-      carritoContenedor.innerHTML = mensaje;
+// Lista de todos los contenedores de productos
+const productsList = document.querySelector('.container-items');
 
-      let quiereEliminarProducto = prompt(mensaje + "\nDesea eliminar algun producto? Presione '1' para Si o '2' para no.");
-      switch (quiereEliminarProducto) {
-          case "1":
-              eliminarProductoDelCarrito();
-              break;
-          case "2":
-              break;
-          default:
-              alert("Elija una opción correcta.")
-              break;
-      }
-  }
-}
+// Variable de arreglos de Productos
+let allProducts = [];
 
-// Agregar un evento de clic al enlace "Carrito"
-var carritoLink = document.getElementById("#carrito");
-carritoLink.addEventListener("click", mostrarCarrito());
+const valorTotal = document.querySelector('.total-pagar');
 
-const clickboton = document.getElementsByClassName(".btn-submit");
-clickboton.addEventListener("click", () => {
-  alert("HI")
-})
+const countProducts = document.querySelector('#contador-productos');
 
-function eliminarProductoDelCarrito() {
-  if (listaCarrito.length === 0) {
-    alert("El carrito está vacío.");
-    return;
-  }
-  let productoAEliminar = parseInt(prompt("Ingrese el número de ID del producto que desea eliminar del carrito:"));
-  const IDProductoBorrado = listaCarrito.findIndex(el => el.id === productoAEliminar);
-  if (IDProductoBorrado != -1) {
-    carrito -= listaCarrito[IDProductoBorrado].precio;
-    alert("Producto: " + listaCarrito[IDProductoBorrado].nombre + " " + listaCarrito[IDProductoBorrado].tipo + " borrado con exito.")
-    listaCarrito.splice(IDProductoBorrado,1);
-  } else {
-    alert("Producto no encontrado.")
-  }
-}
+const cartEmpty = document.querySelector('.cart-empty');
+const cartTotal = document.querySelector('.cart-total');
 
-function depositarSaldo(){
-  let depositar = parseFloat(prompt("Usted tiene: $" + saldo + " pesos en su cuenta. \nIntroduzca el monto que desee depositar: "));
-  saldo += depositar;
-  alert("Su nuevo saldo en su cuenta es de: $" + saldo + " pesos.");
-}
+productsList.addEventListener('click', e => {
+	if (e.target.classList.contains('btn-add-cart')) {
+		const product = e.target.parentElement;
 
-function pagarProductos() {
-  let pago = prompt("Su cuenta dispone de: $" + saldo + " pesos. El monto a pagar es de: $" + carrito + " pesos. \nPresione '1' para pagar, '2' para volver atras o '3' para depositar en su cuenta.");
-  switch(pago) {
-    case "1":
-      if (carrito > saldo) {
-        alert("El saldo en su cuenta es inferior al monto que tiene en el carrito. Deposite plata.");
-        break;
-      } else if (carrito <= 0){
-        alert("No tiene nada en el carrito para pagar.");
-        break;
-      } else {
-        saldo -= carrito;
-        listaCarrito.length = 0;
-        carrito = 0;
-        alert("Pago exitoso. Su nuevo saldo en su cuenta es de: $" + saldo + " pesos.");
-        break;
-      }
-    case "2":
-      break;
-    case "3":
-      depositarSaldo()
-      break;
-  }
-}
+		const infoProduct = {
+			quantity: 1,
+			title: product.querySelector('h2').textContent,
+			price: product.querySelector('p').textContent,
+		};
 
-/* * * * * * * CODIGO * * * * * * * */
-let opcion = prompt("Elija lo que desee hacer a continuacion: \n 1- Comprar ofertas exclusivas de la semana. \n 2- Carrito. \n 3- Depositar. \n 4- Pagar. \n 5- Salir");
-while (opcion != 5) {
-  switch(opcion) {
-    case "1":
-      ejecutarConMiembro(comprarProducto);
-      break;
+		const exits = allProducts.some(
+			product => product.title === infoProduct.title
+		);
 
-    case "2":
-      mostrarCarrito();
-      break;
+		if (exits) {
+			const products = allProducts.map(product => {
+				if (product.title === infoProduct.title) {
+					product.quantity++;
+					return product;
+				} else {
+					return product;
+				}
+			});
+			allProducts = [...products];
+		} else {
+			allProducts = [...allProducts, infoProduct];
+		}
 
-    case "3":
-      depositarSaldo();
-      break;
+		showHTML();
+	}
+});
 
-    case "4":
-      pagarProductos();
-      break;
+rowProduct.addEventListener('click', e => {
+	if (e.target.classList.contains('icon-close')) {
+		const product = e.target.parentElement;
+		const title = product.querySelector('p').textContent;
 
-    default:
-      alert("Elija una opcion valida.");
-      break;
+		allProducts = allProducts.filter(
+			product => product.title !== title
+		);
 
-  }
-  opcion = prompt("Elija lo que desee hacer a continuacion: \n 1- Comprar ofertas exclusivas de la semana. \n 2- Carrito. \n 3- Depositar. \n 4- Pagar. \n 5- Salir");
-}
+		console.log(allProducts);
+
+		showHTML();
+	}
+});
+
+// Funcion para mostrar  HTML
+const showHTML = () => {
+	if (!allProducts.length) {
+		cartEmpty.classList.remove('hidden');
+		rowProduct.classList.add('hidden');
+		cartTotal.classList.add('hidden');
+	} else {
+		cartEmpty.classList.add('hidden');
+		rowProduct.classList.remove('hidden');
+		cartTotal.classList.remove('hidden');
+	}
+
+	// Limpiar HTML
+	rowProduct.innerHTML = '';
+
+	let total = 0;
+	let totalOfProducts = 0;
+
+	allProducts.forEach(product => {
+		const containerProduct = document.createElement('div');
+		containerProduct.classList.add('cart-product');
+
+		containerProduct.innerHTML = `
+            <div class="info-cart-product">
+                <span class="cantidad-producto-carrito">${product.quantity}</span>
+                <p class="titulo-producto-carrito">${product.title}</p>
+                <span class="precio-producto-carrito">${product.price}</span>
+            </div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="icon-close"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                />
+            </svg>
+        `;
+
+		rowProduct.append(containerProduct);
+
+		total = total + parseInt(product.quantity * product.price.slice(1));
+    totalOfProducts = totalOfProducts + product.quantity;
+	});
+
+	valorTotal.innerText = `$${total}`;
+	countProducts.innerText = totalOfProducts;
+};
