913b67755efbface82e947bed55d78445f0d9a21
Jorge Lautaro Silvestro <lautarosilvestro2000@hotmail.com>
2023-09-23 18:59:21 -0300

    proyectofinal

--- js/main.js
+++ js/main.js
@@ -25,6 +25,18 @@ const cartEmpty = document.querySelector('.cart-empty');
 const cartTotal = document.querySelector('.cart-total');
 
 productsList.addEventListener('click', e => {
+
+	Swal.fire({
+		position: 'bottom-end',
+		icon: 'success',
+		title: 'Producto agregado al carrito',
+		showConfirmButton: false,
+		timer: 900,
+		allowOutsideClick: true, 
+		backdrop: false,
+		toast: true, 
+	});
+
 	if (e.target.classList.contains('btn-add-cart')) {
 		const product = e.target.parentElement;
 
@@ -117,10 +129,77 @@ const showHTML = () => {
 
 		rowProduct.append(containerProduct);
 
-		total = total + parseInt(product.quantity * product.price.slice(1));
+		total = total + parseFloat(product.quantity * product.price.slice(1));
     totalOfProducts = totalOfProducts + product.quantity;
 	});
 
 	valorTotal.innerText = `$${total}`;
 	countProducts.innerText = totalOfProducts;
 };
+
+
+const btnPagar = document.querySelector('.pagar-carrito');
+
+btnPagar.addEventListener('click', () => {
+	
+	if (allProducts.length === 0) {
+		Swal.fire({
+		  title: 'El carrito está vacío',
+		  icon: 'warning',
+		  confirmButtonText: 'OK',
+		});
+		return;
+	  }
+
+	Swal.fire({
+	  title: 'Ingresa los datos de tu tarjeta',
+	  html:
+		'<input type="text" id="numeroTarjeta" class="swal2-input" placeholder="Número de tarjeta" inputmode="numeric" pattern="[0-9]*" minlength="16" maxlength="16">' +
+		'<input type="text" id="codigoSeguridad" class="swal2-input" placeholder="Código de seguridad" inputmode="numeric" pattern="[0-9]*" minlength="3" maxlength="4">' +
+		'<input type="text" id="fechaVencimiento" class="swal2-input" placeholder="Fecha de vencimiento (MM/AA)" inputmode="numeric" pattern="[0-9/]*" minlength="5" maxlength="5">',
+	  confirmButtonText: 'Pagar',
+	  showCancelButton: true,
+	  cancelButtonText: 'Cancelar',
+	  preConfirm: () => {
+		const numeroTarjeta = document.getElementById('numeroTarjeta').value;
+		const codigoSeguridad = document.getElementById('codigoSeguridad').value;
+		const fechaVencimiento = document.getElementById('fechaVencimiento').value;
+  
+		allProducts = [];
+
+		return fetch('https://jsonplaceholder.typicode.com/posts', {
+		  method: 'POST',
+		  body: JSON.stringify({
+			numeroTarjeta,
+			codigoSeguridad,
+			fechaVencimiento,
+		  }),
+		  headers: {
+			'Content-Type': 'application/json',
+		  },
+		})
+		.then(response => {
+		  if (!response.ok) {
+			throw new Error('Error en la operación de pago');
+		  }
+		  return response.json();
+		})
+		.catch(error => {
+		  Swal.fire({
+			title: 'Error en el pago',
+			text: error.message,
+			icon: 'error',
+			confirmButtonText: 'OK',
+		  });
+		});
+	  },
+	}).then(() => {
+	  Swal.fire({
+		title: '¡Gracias por comprar con nosotros!',
+		text: 'Disfrute de sus productos',
+		icon: 'success',
+	  });
+	}).finally(() => {
+	  console.log('Operación finalizada.');
+	});
+  });
